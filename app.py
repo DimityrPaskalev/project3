0a3b201b071154ae34b14a85efca0b067b551690
DimityrPaskalev <ddpaskalev24a@gmail.com>
2026-01-22 09:32:25 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title("📝 Анкета")
+name = st.text_input("Въведи име:")
+age = st.number_input(
+    "Въведи възраст:",
+    min_value=1,
+    max_value=120,
+    step=1
+)
+rating = st.selectbox(
+    "Дай оценка:",
+    [1, 2, 3, 4, 5]
+)
+if st.button("Изпрати анкетата"):
+    if name.strip() == "":
+        st.error("Моля, въведи име!")
+    else:
+        st.success("Анкетата е изпратена успешно!")
+        st.write("### Резултат:")
+        st.write(f"**Име:** {name}")
+        st.write(f"**Възраст:** {age}")
+        st.write(f"**Оценка:** {rating}/5")
